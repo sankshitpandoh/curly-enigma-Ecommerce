12ba85e1b765d60b7148541f3d521a22e9d8ff6f
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-07-06 16:20:12 +0530

    added reset css , font face and bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './Stylesheets/bootstrap.min.css';
+import './Stylesheets/fontFace.css';
+import './Stylesheets/reset.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <>  
+      </>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
